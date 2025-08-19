0df5810a05b2e621b35f08fda6e702867be899ca
IronMan2297 <liamweigel1@icloud.com>
2025-08-19 22:14:32 +0200

    custom chainsaw

--- src/main/java/net/ironman/mccourse/item/ModItems.java
+++ src/main/java/net/ironman/mccourse/item/ModItems.java
@@ -3,6 +3,7 @@ package net.ironman.mccourse.item;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.ironman.mccourse.MCCourseMod;
+import net.ironman.mccourse.item.custom.ChainsawItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -12,6 +13,8 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item FLUORITE = registerItem("fluorite", new Item(new Item.Settings()));
     public static final Item RAW_FLUORITE = registerItem("raw_fluorite", new Item(new Item.Settings()));
+
+    public static final Item CHAINSAW = registerItem("chainsaw", new ChainsawItem(new Item.Settings().maxDamage(128)));
 
 
     private static Item registerItem(String name, Item item) {
