572f35879513140bdbc9eecf6998eedeee914fdb
IronMan2297 <liamweigel1@icloud.com>
2025-08-24 00:24:49 +0200

    datagen

---
+++ src/main/java/net/ironman/mccourse/datagen/ModItemTagProvider.java
@@ -0,0 +1,28 @@
+package net.ironman.mccourse.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.ironman.mccourse.item.ModItems;
+import net.ironman.mccourse.util.ModTags;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.FLUORITE)
+                .add(ModItems.RAW_FLUORITE)
+                .add(Items.COAL)
+                .add(Items.STICK)
+                .add(Items.APPLE);
+
+    }
+}
---
+++ src/main/java/net/ironman/mccourse/datagen/ModRecipeGenerator.java
@@ -0,0 +1,43 @@
+package net.ironman.mccourse.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.ironman.mccourse.block.ModBlocks;
+import net.ironman.mccourse.item.ModItems;
+import net.minecraft.block.Blocks;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeGenerator extends FabricRecipeProvider {
+    public ModRecipeGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    public void generate(RecipeExporter recipeExporter) {
+        List<ItemConvertible> FLUORITE_SMELTABLES = List.of(ModItems.RAW_FLUORITE, ModBlocks. FLUORITE_ORE,
+                ModBlocks. FLUORITE_DEEPSLATE_ORE, ModBlocks. FLUORITE_NETHER_ORE, ModBlocks. FLUORITE_END_ORE);
+
+        offerSmelting(recipeExporter, FLUORITE_SMELTABLES, RecipeCategory.MISC, ModItems.FLUORITE, 0.2F, 200, "fluorite");
+        offerBlasting(recipeExporter, FLUORITE_SMELTABLES, RecipeCategory.MISC, ModItems.FLUORITE, 0.2F, 100, "fluorite");
+
+        offerReversibleCompactingRecipes(recipeExporter, RecipeCategory.BUILDING_BLOCKS, ModItems.FLUORITE, RecipeCategory.DECORATIONS, ModBlocks.FLUORITE_BLOCK);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.RAW_FLUORITE)
+                .pattern("SSS")
+                .pattern("SFS")
+                .pattern("SSS")
+                .input('S', Blocks.STONE)
+                .input('F', ModItems.FLUORITE)
+                .criterion(hasItem(Blocks.STONE), conditionsFromItem(Blocks.STONE))
+                .criterion(hasItem(ModItems.FLUORITE), conditionsFromItem(ModItems.FLUORITE))
+                .offerTo(recipeExporter);       //.offerTo(recipeExporter, Identifier.of(MCCourseMod.MOD_ID, "raw_fluorite_2")); custom name
+    }
+}
